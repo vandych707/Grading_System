ce70efc6c62b2fda9b142893e893f1adb7cf81ba
vandych707 <143641846+vandych707@users.noreply.github.com>
2024-01-08 22:34:25 +0530

    initial commit

---
+++ Marksheet.c
@@ -0,0 +1,38 @@
+/*#include<stdio.h>
+int main(){
+   int num;
+   printf("Enter your Total Percentage :");
+   scanf("%d",&num);
+   printf("You are entered %d\n",num);
+    
+            if(num>=80){
+               if (num>=80 && num<=85){
+                   printf("You got a grade 'A'\n");
+               }
+               else if (num>85 && num<=90){
+                   printf("You got a grade 'A+'");
+               }
+               else{
+                   printf("YOU got a grade 'Outstanding'");
+               }
+                ;
+            }
+            else if(num>=60){
+                if(num>=60 && num<=70){
+                    printf("You got a grade 'B'");
+            }
+                else 
+                printf("You got a grade 'B+'");
+            }
+            else if (num>=40){
+                    if(num>=40 && num<=50 ){
+                    printf("You got a grade 'C'");
+                }
+                    else
+                    printf("Your got a grade 'C+'");
+            }
+            else if (num<40){
+                printf("You failed in this exam");
+            }
+    return 0;
+} */
