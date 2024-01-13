7d3b53520227414fc6f0db6cda8f55ee3b74761c
vandych707 <143641846+vandych707@users.noreply.github.com>
2024-01-13 13:24:19 +0530

    Arthemic and relational operator

---
+++ Relational.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    a = 35;
+    b = 42;
+    printf("a is equal to  b that false (0))= %d\n", a == b);
+    printf("a is not equal to  b= %d\n", a != b);
+    printf("a greater than b= %d\n", a > b);
+    printf("a less than b %d\n", a < b);
+    printf("a greater than equal to b %d\n", a >= b);
+    printf("a less than equal to b %d\n", a <= b);
+    return 0;
+}
